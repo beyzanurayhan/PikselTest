96824a863516fa79ae38d1b784f872491e155615
Beyza <byzanurayhan@gmail.com>
2022-07-16 16:52:26 +0300

    Add project files.

---
+++ PikselTest/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PikselTest
+{
+    partial class PikselTestForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_black = new System.Windows.Forms.Button();
+            this.btn_white = new System.Windows.Forms.Button();
+            this.btn_red = new System.Windows.Forms.Button();
+            this.btn_green = new System.Windows.Forms.Button();
+            this.btn_blue = new System.Windows.Forms.Button();
+            this.btn_yellow = new System.Windows.Forms.Button();
+            this.btn_turquoise = new System.Windows.Forms.Button();
+            this.btn_purple = new System.Windows.Forms.Button();
+            this.btn_gray = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_black
+            // 
+            this.btn_black.BackColor = System.Drawing.Color.Black;
+            this.btn_black.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_black.Location = new System.Drawing.Point(12, 12);
+            this.btn_black.Name = "btn_black";
+            this.btn_black.Size = new System.Drawing.Size(110, 110);
+            this.btn_black.TabIndex = 3;
+            this.btn_black.UseVisualStyleBackColor = false;
+            this.btn_black.Click += new System.EventHandler(this.btn_black_Click);
+            this.btn_black.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_black_KeyUp);
+            // 
+            // btn_white
+            // 
+            this.btn_white.BackColor = System.Drawing.Color.White;
+            this.btn_white.Location = new System.Drawing.Point(138, 12);
+            this.btn_white.Name = "btn_white";
+            this.btn_white.Size = new System.Drawing.Size(110, 110);
+            this.btn_white.TabIndex = 4;
+            this.btn_white.UseVisualStyleBackColor = false;
+            this.btn_white.Click += new System.EventHandler(this.btn_white_Click);
+            this.btn_white.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_white_KeyUp);
+            // 
+            // btn_red
+            // 
+            this.btn_red.BackColor = System.Drawing.Color.Red;
+            this.btn_red.Location = new System.Drawing.Point(257, 12);
+            this.btn_red.Name = "btn_red";
+            this.btn_red.Size = new System.Drawing.Size(110, 110);
+            this.btn_red.TabIndex = 5;
+            this.btn_red.UseVisualStyleBackColor = false;
+            this.btn_red.Click += new System.EventHandler(this.btn_red_Click);
+            this.btn_red.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_red_KeyUp);
+            // 
+            // btn_green
+            // 
+            this.btn_green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_green.Location = new System.Drawing.Point(12, 144);
+            this.btn_green.Name = "btn_green";
+            this.btn_green.Size = new System.Drawing.Size(110, 110);
+            this.btn_green.TabIndex = 6;
+            this.btn_green.UseVisualStyleBackColor = false;
+            this.btn_green.Click += new System.EventHandler(this.btn_green_Click);
+            this.btn_green.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_green_KeyUp);
+            // 
+            // btn_blue
+            // 
+            this.btn_blue.BackColor = System.Drawing.Color.Blue;
+            this.btn_blue.Location = new System.Drawing.Point(138, 144);
+            this.btn_blue.Name = "btn_blue";
+            this.btn_blue.Size = new System.Drawing.Size(110, 110);
+            this.btn_blue.TabIndex = 7;
+            this.btn_blue.UseVisualStyleBackColor = false;
+            this.btn_blue.Click += new System.EventHandler(this.btn_blue_Click);
+            this.btn_blue.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_blue_KeyUp);
+            // 
+            // btn_yellow
+            // 
+            this.btn_yellow.BackColor = System.Drawing.Color.Yellow;
+            this.btn_yellow.Location = new System.Drawing.Point(257, 144);
+            this.btn_yellow.Name = "btn_yellow";
+            this.btn_yellow.Size = new System.Drawing.Size(110, 110);
+            this.btn_yellow.TabIndex = 8;
+            this.btn_yellow.UseVisualStyleBackColor = false;
+            this.btn_yellow.Click += new System.EventHandler(this.btn_yellow_Click);
+            this.btn_yellow.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_yellow_KeyUp);
+            // 
+            // btn_turquoise
+            // 
+            this.btn_turquoise.BackColor = System.Drawing.Color.Cyan;
+            this.btn_turquoise.Location = new System.Drawing.Point(12, 275);
+            this.btn_turquoise.Name = "btn_turquoise";
+            this.btn_turquoise.Size = new System.Drawing.Size(110, 110);
+            this.btn_turquoise.TabIndex = 9;
+            this.btn_turquoise.UseVisualStyleBackColor = false;
+            this.btn_turquoise.Click += new System.EventHandler(this.btn_turquoise_Click);
+            this.btn_turquoise.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_turquoise_KeyUp);
+            // 
+            // btn_purple
+            // 
+            this.btn_purple.BackColor = System.Drawing.Color.Fuchsia;
+            this.btn_purple.Location = new System.Drawing.Point(138, 275);
+            this.btn_purple.Name = "btn_purple";
+            this.btn_purple.Size = new System.Drawing.Size(110, 110);
+            this.btn_purple.TabIndex = 10;
+            this.btn_purple.UseVisualStyleBackColor = false;
+            this.btn_purple.Click += new System.EventHandler(this.btn_purple_Click);
+            this.btn_purple.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_purple_KeyUp);
+            // 
+            // btn_gray
+            // 
+            this.btn_gray.BackColor = System.Drawing.Color.Gray;
+            this.btn_gray.Location = new System.Drawing.Point(257, 275);
+            this.btn_gray.Name = "btn_gray";
+            this.btn_gray.Size = new System.Drawing.Size(110, 110);
+            this.btn_gray.TabIndex = 11;
+            this.btn_gray.UseVisualStyleBackColor = false;
+            this.btn_gray.Click += new System.EventHandler(this.btn_gray_Click);
+            this.btn_gray.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_gray_KeyUp);
+            // 
+            // PikselTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 398);
+            this.Controls.Add(this.btn_gray);
+            this.Controls.Add(this.btn_purple);
+            this.Controls.Add(this.btn_turquoise);
+            this.Controls.Add(this.btn_yellow);
+            this.Controls.Add(this.btn_blue);
+            this.Controls.Add(this.btn_green);
+            this.Controls.Add(this.btn_red);
+            this.Controls.Add(this.btn_white);
+            this.Controls.Add(this.btn_black);
+            this.MinimumSize = new System.Drawing.Size(410, 437);
+            this.Name = "PikselTestForm";
+            this.Text = "Ölü Piksel Testi";
+            this.Load += new System.EventHandler(this.PikselTestForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button btn_black;
+        private Button btn_white;
+        private Button btn_red;
+        private Button btn_green;
+        private Button btn_blue;
+        private Button btn_yellow;
+        private Button btn_turquoise;
+        private Button btn_purple;
+        private Button btn_gray;
+    }
+}
---
+++ PikselTest/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System.Diagnostics;
+
+namespace PikselTest
+{
+    public partial class PikselTestForm : Form
+    {
+        public PikselTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PikselTestForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test işlemine başlamadan önce lütfen ekranınızın temiz olduğundan emin olunuz.");
+        }
+        
+
+        private void btn_black_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_black.Dock = DockStyle.Fill;
+            btn_white.Visible = false;
+            btn_red.Visible = false;
+            btn_green.Visible = false;
+            btn_blue.Visible = false;
+            btn_yellow.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_purple.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+
+        }
+
+        private void btn_white_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_white.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_red.Visible = false;
+            btn_green.Visible = false;
+            btn_blue.Visible = false;
+            btn_yellow.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_purple.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+        private void btn_red_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_red.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_white.Visible = false;
+            btn_green.Visible = false;
+            btn_blue.Visible = false;
+            btn_yellow.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_purple.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+        private void btn_green_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_green.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_red.Visible = false;
+            btn_white.Visible = false;
+            btn_blue.Visible = false;
+            btn_yellow.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_purple.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+        private void btn_blue_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_blue.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_red.Visible = false;
+            btn_green.Visible = false;
+            btn_white.Visible = false;
+            btn_yellow.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_purple.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+        private void btn_yellow_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_yellow.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_red.Visible = false;
+            btn_green.Visible = false;
+            btn_blue.Visible = false;
+            btn_white.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_purple.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+        private void btn_turquoise_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_turquoise.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_red.Visible = false;
+            btn_green.Visible = false;
+            btn_blue.Visible = false;
+            btn_yellow.Visible = false;
+            btn_white.Visible = false;
+            btn_purple.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+        private void btn_purple_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_purple.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_red.Visible = false;
+            btn_green.Visible = false;
+            btn_blue.Visible = false;
+            btn_yellow.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_white.Visible = false;
+            btn_gray.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+        private void btn_gray_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            btn_gray.Dock = DockStyle.Fill;
+            btn_black.Visible = false;
+            btn_red.Visible = false;
+            btn_green.Visible = false;
+            btn_blue.Visible = false;
+            btn_yellow.Visible = false;
+            btn_turquoise.Visible = false;
+            btn_purple.Visible = false;
+            btn_white.Visible = false;
+            MessageBox.Show("Çıkmak için escape tuşuna basınız.");
+        }
+
+
+        private void btn_black_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_black.Dock = DockStyle.None;
+                btn_white.Visible = true;
+                btn_red.Visible = true;
+                btn_green.Visible = true;
+                btn_blue.Visible = true;
+                btn_yellow.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_purple.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_white_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_white.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_red.Visible = true;
+                btn_green.Visible = true;
+                btn_blue.Visible = true;
+                btn_yellow.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_purple.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_red_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_red.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_white.Visible = true;
+                btn_green.Visible = true;
+                btn_blue.Visible = true;
+                btn_yellow.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_purple.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_green_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_green.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_white.Visible = true;
+                btn_red.Visible = true;
+                btn_blue.Visible = true;
+                btn_yellow.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_purple.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_blue_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_blue.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_white.Visible = true;
+                btn_green.Visible = true;
+                btn_red.Visible = true;
+                btn_yellow.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_purple.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_yellow_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_yellow.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_white.Visible = true;
+                btn_green.Visible = true;
+                btn_blue.Visible = true;
+                btn_red.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_purple.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_turquoise_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_turquoise.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_white.Visible = true;
+                btn_green.Visible = true;
+                btn_blue.Visible = true;
+                btn_yellow.Visible = true;
+                btn_red.Visible = true;
+                btn_purple.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_purple_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_purple.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_white.Visible = true;
+                btn_green.Visible = true;
+                btn_blue.Visible = true;
+                btn_yellow.Visible = true;
+                btn_red.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_gray.Visible = true;
+            }
+        }
+
+        private void btn_gray_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                btn_gray.Dock = DockStyle.None;
+                btn_black.Visible = true;
+                btn_white.Visible = true;
+                btn_green.Visible = true;
+                btn_blue.Visible = true;
+                btn_yellow.Visible = true;
+                btn_red.Visible = true;
+                btn_turquoise.Visible = true;
+                btn_purple.Visible = true;
+            }
+        }
+    }
+}
